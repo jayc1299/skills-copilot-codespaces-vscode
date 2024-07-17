ee10a7e4c2cfe3bb24ad9b0e43aeeab89a6c21cb
Jay Cunnington <jay.cunnington@gmail.com>
2024-07-17 09:27:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,21 @@
+function skillsMember() {
+    var skills = [
+        {
+            skill: 'HTML',
+            level: 5
+        },
+        {
+            skill: 'CSS',
+            level: 5
+        },
+        {
+            skill: 'JS',
+            level: 4
+        },
+        {
+            skill: 'React',
+            level: 3
+        }
+    ];
+    return skills;
+}
